94c70d9536120fb1555676ec1b996af52822193a
Patricio Echague <patricioe@gmail.com>
2020-04-14 12:56:06 -0700

    Bilal changes

--- src/main/java/io/split/jenkins/plugins/SplitPluginBuilder.java
+++ src/main/java/io/split/jenkins/plugins/SplitPluginBuilder.java
@@ -17,15 +17,15 @@ import javax.servlet.ServletException;
 import java.io.IOException;
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
-//import org.kohsuke.stapler.DataBoundSetter;
+import org.apache.log4j.Logger;
 import net.sf.json.JSONObject;
 
 /**
-* Class implementing Jenkins plugin builder and descriptor
-*
-* @author Bilal Al-Shshany
-*
-*/
+ * Class implementing Jenkins plugin builder and descriptor
+ *
+ * @author Bilal Al-Shshany
+ *
+ */
 public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
 
     private final String splitTask;
@@ -38,10 +38,12 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
     private final String whitelistKey;
     private final String splitYAMLFile;
     private String apiKey;
+    private static Logger _log = Logger.getLogger(SplitAPI.class);
+
 
     @DataBoundConstructor
     public SplitPluginBuilder(String splitTask, String[] splitName, String[] environmentName, String[] workspaceName, String[] trafficTypeName, String splitDefinitions, String whitelistKey, String treatmentName, String splitYAMLFile) {
-        
+
         this.splitName = splitName.clone();
         this.environmentName = environmentName.clone();
         this.workspaceName = workspaceName.clone();
@@ -54,7 +56,7 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
         this.apiKey = DescriptorImpl.getSplitAdminApiKey();
     }
 
-    private int getWorkspaceIndex(String splitTask) {
+    private int getSplitNameIndex(String splitTask) {
         int index = 0;
         if (splitTask.equals("createSplit")) index = 0;
         if (splitTask.equals("addToEnvironment")) index = 1;
@@ -62,28 +64,39 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
         if (splitTask.equals("killSplit")) index = 3;
         if (splitTask.equals("deleteSplitDefinition")) index = 4;
         if (splitTask.equals("deleteSplit")) index = 5;
-        if (splitTask.equals("createSplitFromYAML")) index = 6;
+        return index;
+    }
+
+    private int getWorkspaceIndex(String splitTask) {
+        int index = 0;
+        if (splitTask.equals("createSplitFromYAML")) index = 0;
+        if (splitTask.equals("createSplit")) index = 1;
+        if (splitTask.equals("addToEnvironment")) index = 2;
+        if (splitTask.equals("addKeyToWhitelist")) index = 3;
+        if (splitTask.equals("killSplit")) index = 4;
+        if (splitTask.equals("deleteSplitDefinition")) index = 5;
+        if (splitTask.equals("deleteSplit")) index = 6;
         return index;
     }
 
     private int getEnvironmentIndex(String splitTask) {
         int index = 0;
-        if (splitTask.equals("addToEnvironment")) index = 0;
-        if (splitTask.equals("addKeyToWhitelist")) index = 1;
-        if (splitTask.equals("killSplit")) index = 2;
-        if (splitTask.equals("deleteSplitDefinition")) index = 3;
-        if (splitTask.equals("createSplitFromYAML")) index = 4;
+        if (splitTask.equals("createSplitFromYAML")) index = 0;
+        if (splitTask.equals("addToEnvironment")) index = 1;
+        if (splitTask.equals("addKeyToWhitelist")) index = 2;
+        if (splitTask.equals("killSplit")) index = 3;
+        if (splitTask.equals("deleteSplitDefinition")) index = 4;
         return index;
     }
 
     private int getTrafficTypeIndex(String splitTask) {
         int index = 0;
-        if (splitTask.equals("createSplit")) index = 0;
-        if (splitTask.equals("createSplitFromYAML")) index = 1;
+        if (splitTask.equals("createSplitFromYAML")) index = 0;
+        if (splitTask.equals("createSplit")) index = 1;
         return index;
     }
 
-    
+
     public String getTrafficTypeName() {
         return trafficTypeName[getTrafficTypeIndex(splitTask)];
     }
@@ -91,21 +104,21 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
     public String getSplitYAMLFile() {
         return splitYAMLFile;
     }
-    
+
     public String getTreatmentName() {
         return treatmentName;
     }
-    
+
     public String getWhitelistKey() {
         return whitelistKey;
     }
-    
+
     public String getSplitDefinitions() {
         return splitDefinitions;
     }
 
     public String getSplitName() {
-        return splitName[getWorkspaceIndex(splitTask)];
+        return splitName[getSplitNameIndex(splitTask)];
     }
 
     public String getEnvironmentName() {
@@ -115,7 +128,7 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
     public String getWorkspaceName() {
         return workspaceName[getWorkspaceIndex(splitTask)];
     }
-    
+
     public String getSplitTask() {
         return splitTask;
     }
@@ -123,21 +136,22 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
     public void setApiKey(String splitApiKey) {
         this.apiKey = splitApiKey;
     }
-    
+
     @Override
     public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
         String splitYAMLFileFullPath = "";
         splitYAMLFileFullPath = workspace.getRemote() + "/" + splitYAMLFile;
-        listener.getLogger().println("Select Task: " + splitTask);
+        _log.info("Selected Task: " + splitTask);
+        listener.getLogger().println("Selected Task: " + splitTask);
         if (this.apiKey.equals("")) this.apiKey = DescriptorImpl.getSplitAdminApiKey();
         SplitAPI spAdmin = new SplitAPI(this.apiKey);
         String workspaceId = spAdmin.getWorkspaceId(workspaceName[getWorkspaceIndex(splitTask)]);
         listener.getLogger().println("WorkspaceId: " + workspaceId);
         Integer statusCode = 0;
         if (splitTask.equals("createSplit")) {
-            statusCode=spAdmin.createSplit(workspaceId, trafficTypeName[getTrafficTypeIndex(splitTask)], splitName[getWorkspaceIndex(splitTask)], "Created From Jenkins Split Admin API");
+            statusCode=spAdmin.createSplit(workspaceId, trafficTypeName[getTrafficTypeIndex(splitTask)], splitName[getSplitNameIndex(splitTask)], "Created From Jenkins Split Admin API");
             listener.getLogger().println("Returned Status Code: " + statusCode.toString());
-            listener.getLogger().println("Split (" + splitName[getWorkspaceIndex(splitTask)] + ") is created!");
+            listener.getLogger().println("Split (" + splitName[getSplitNameIndex(splitTask)] + ") is created!");
         }
         if (splitTask.equals("createSplitFromYAML")) {
             statusCode=spAdmin.createSplitFromYAML(workspaceId, environmentName[getEnvironmentIndex(splitTask)], trafficTypeName[getTrafficTypeIndex(splitTask)], splitYAMLFileFullPath);
@@ -145,32 +159,34 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
             listener.getLogger().println("Splits created successfully from (" + splitYAMLFileFullPath + ")!");
         }
         if (splitTask.equals("addToEnvironment")) {
-            statusCode=spAdmin.addSplitToEnvironment(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getWorkspaceIndex(splitTask)],  splitDefinitions);
+            statusCode=spAdmin.addSplitToEnvironment(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getSplitNameIndex(splitTask)],  splitDefinitions);
             listener.getLogger().println("Returned Status Code: " + statusCode.toString());
-            listener.getLogger().println("Split (" + splitName[getWorkspaceIndex(splitTask)] + ") is added to (" + environmentName[getEnvironmentIndex(splitTask)] + ") Environment!");
+            listener.getLogger().println("Split (" + splitName[getSplitNameIndex(splitTask)] + ") is added to (" + environmentName[getEnvironmentIndex(splitTask)] + ") Environment!");
         }
         if (splitTask.equals("killSplit")) {
-            statusCode=spAdmin.killSplit(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getWorkspaceIndex(splitTask)]);
+            statusCode=spAdmin.killSplit(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getSplitNameIndex(splitTask)]);
             listener.getLogger().println("Returned Status Code: " + statusCode.toString());
-            listener.getLogger().println("Split (" + splitName[getWorkspaceIndex(splitTask)] + ") is killed!");
+            listener.getLogger().println("Split (" + splitName[getSplitNameIndex(splitTask)] + ") is killed!");
         }
         if (splitTask.equals("addKeyToWhitelist")) {
-            statusCode=spAdmin.addWhiteListToSplit(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getWorkspaceIndex(splitTask)],  treatmentName, whitelistKey);
+            statusCode=spAdmin.addWhiteListToSplit(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getSplitNameIndex(splitTask)],  treatmentName, whitelistKey);
             listener.getLogger().println("Returned Status Code: " + statusCode.toString());
-            listener.getLogger().println("Key (" + whitelistKey + ") is added to ("+treatmentName+") Whitelist in Split (" + splitName[getWorkspaceIndex(splitTask)] + ")");
+            listener.getLogger().println("Key (" + whitelistKey + ") is added to ("+treatmentName+") Whitelist in Split (" + splitName[getSplitNameIndex(splitTask)] + ")");
         }
         if (splitTask.equals("deleteSplit")) {
-            statusCode=spAdmin.deleteSplit(workspaceId, splitName[getWorkspaceIndex(splitTask)]);
+            statusCode=spAdmin.deleteSplit(workspaceId, splitName[getSplitNameIndex(splitTask)]);
             listener.getLogger().println("Returned Status Code: " + statusCode.toString());
-            listener.getLogger().println("Split (" + splitName[getWorkspaceIndex(splitTask)] + ") is deleted!");
+            listener.getLogger().println("Split (" + splitName[getSplitNameIndex(splitTask)] + ") is deleted!");
         }
         if (splitTask.equals("deleteSplitDefinition")) {
-            statusCode=spAdmin.deleteSplitDefinition(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getWorkspaceIndex(splitTask)]);
+            statusCode=spAdmin.deleteSplitDefinition(workspaceId, environmentName[getEnvironmentIndex(splitTask)], splitName[getSplitNameIndex(splitTask)]);
             listener.getLogger().println("Returned Status Code: " + statusCode.toString());
-            listener.getLogger().println("Split (" + splitName[getWorkspaceIndex(splitTask)] + ") definition is deleted!");
+            listener.getLogger().println("Split (" + splitName[getSplitNameIndex(splitTask)] + ") definition is deleted!");
         }
+        _log.info("Task:" + splitTask + " is completed");
+
     }
-    
+
     @Symbol("greet")
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
@@ -179,11 +195,11 @@ public class SplitPluginBuilder extends Builder implements SimpleBuildStep {
         public static String getSplitAdminApiKey() {
             return splitAdminApiKey;
         }
-        
+
         public static void setSplitAdminApiKey(String splitApiKey) {
             splitAdminApiKey = splitApiKey;
         }
-        
+
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData)
                 throws FormException {
