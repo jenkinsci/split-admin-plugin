3cec60f4e704bb4d67fbde16c6f0d328ad743c81
chillaq <bilal.al-shahwany@split.io>
2020-04-19 13:55:21 -0700

    UI support for Base URL

--- src/main/resources/io/split/jenkins/plugins/SplitPluginBuilder/global.groovy
+++ src/main/resources/io/split/jenkins/plugins/SplitPluginBuilder/global.groovy
@@ -1,8 +1,11 @@
-package io.split.jenkins.plugins.SplitPluginBuilder
-
 f = namespace("/lib/form")
 f.section(title: _("Split Admin Plugin")) {
 	f.entry(field: "splitAdminApiKey", title: _("Split Admin API Key")) {
     		input(type: "text", class: "setting-input", value: descriptor.splitAdminApiKey, name: "ext_split_admin_api_key")
+	}
+	f.advanced() {
+		f.entry(field: "adminBaseURL", title: _("Split Admin API Base URL")) {
+    		input(type: "text", class: "setting-input", value: descriptor.splitAdminBaseURL, name: "ext_admin_base_url")
+		}
 	}
 }
